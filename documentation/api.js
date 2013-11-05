c1454a627992d965040868f576e4f265dcc91f92
weissger <weissger@fim.uni-passau.de>
2013-11-05 23:59:02 +0100

    Added missing files

---
+++ documentation/api.js
@@ -0,0 +1,33 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Cache",
+        "Plugin",
+        "Plugin.DetailView",
+        "Plugin.EventManager",
+        "Plugin.InitView",
+        "Plugin.LayoutEngine",
+        "Plugin.Preview",
+        "Plugin.PreviewInlay",
+        "Plugin.PreviewOverlay",
+        "Plugin.RdfStore",
+        "Plugin.RemoteDataLoader",
+        "Plugin.TemplatesLoader",
+        "Plugin.View",
+        "RemoteEngine",
+        "VisaRDF_GLOBAL",
+        "VisaRDF_GLOBAL_UTIL",
+        "VisaRDF_JQuery_Custom"
+    ],
+    "modules": [
+        "VisaRDF"
+    ],
+    "allModules": [
+        {
+            "displayName": "VisaRDF",
+            "name": "VisaRDF",
+            "description": "VisaRDF is a JQuery Plugin for RDF visualization."
+        }
+    ]
+} };
+});
